5835af6e09c8ec7f327b6508e249ed3408779fe6
SebiCreator <Sebastian.Kaeser.public@gmail.com>
2022-04-01 08:56:44 +0200

    added addressable board

--- src/main/scala/de/htwg/se/dame/Dame.scala
+++ src/main/scala/de/htwg/se/dame/Dame.scala
@@ -1,7 +1,33 @@
 @main def hello: Unit = 
-  println(fullBoardWrapped2(1,1,"O"))
+  val l1 = List(("A","B"),("C","D"),("E","F"),("G","H"))
+  val l2 = List(("X","X"),("X","X"),("X","X"),("X","X"))
+  val l3 = List(("X","X"),("X","X"),("X","X"),("X","X"))
+  val l4 = List(("H","G"),("F","E"),("D","C"),("B","A"))
+  val e1 = List(l1,l2,l3,l4)
+
+  val l5 = List(("A","B"),("C","D"),("E","F"),("G","H"),("A","B"),("C","D"),("E","F"),("G","H"))
+  val l6 = List(("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"))
+  val l7 = List(("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"))
+  val l8 = List(("H","G"),("F","E"),("D","C"),("B","A"),("H","G"),("F","E"),("D","C"),("B","A"))
+  val l9 = List(("H","G"),("F","E"),("D","C"),("B","A"),("H","G"),("F","E"),("D","C"),("B","A"))
+  val l10 = List(("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"))
+  val l11 = List(("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"),("X","X"))
+  val l12 = List(("A","B"),("C","D"),("E","F"),("G","H"),("A","B"),("C","D"),("E","F"),("G","H"))
+  val e2 = List(l5,l6,l7,l8,l9,l10,l11,l12)
+
+  //println(modLine(4,l))
+  //println(modBlock(3,2,l1))
+  //println(modBoardWrapped(5,4,e1))
+  println(modBoardWrapped(5,8,e2))
+  //println(bBlockFull(3,2,"X"))
+
 
 var eol = sys.props("line.separator")
+
+// Helper
+def calcTotalWidth(cellsize: Int, nFields: Int) = (cellsize * nFields) + 2
+def makeodd(number: Int) = (number*2) + 1
+def halfFloor(num: Int) = (num/2).toInt  
 
 // Lines & Cells
 def bLine(cellsize: Int, nfields: Int) = "|" + (("#" * cellsize) + (" " * cellsize))*halfFloor(nfields) + "|" + eol
@@ -9,16 +35,14 @@ def wLine(cellsize: Int, nfields: Int) = "|" + ((" " * cellsize) + ("#" * cellsi
 def innerWCell(cellsize: Int,symbol: String) = " " * (cellsize/2).toInt + symbol + " " * (cellsize/2).toInt
 def innerBCell(cellsize: Int,symbol: String) = "#" * ((cellsize/2).toInt) + symbol + "#" * ((cellsize/2).toInt) 
 
+
+
 // Lines & Blocks with symbols
-def halfFloor(num: Int) = (num/2).toInt  
 def bLineFull(cellsize: Int, nFields: Int, symbol: String) = "|" + (innerBCell(cellsize,symbol) + innerWCell(cellsize,symbol)) * halfFloor(nFields) + "|" +  eol 
 def wLineFull(cellsize: Int, nFields: Int, symbol: String) = "|" + (innerWCell(cellsize,symbol) + innerBCell(cellsize,symbol)) * halfFloor(nFields) + "|" + eol 
 def bBlockFull(cellsize: Int, nFields: Int, symbol: String) = (bLine(cellsize,nFields) * (cellsize/2).toInt) +  bLineFull(cellsize,nFields,symbol) + (bLine(cellsize,nFields) * (cellsize/2).toInt)
 def wBlockFull(cellsize: Int, nFields: Int, symbol: String) = (wLine(cellsize,nFields) * (cellsize/2).toInt) +  wLineFull(cellsize,nFields,symbol) + (wLine(cellsize,nFields) * (cellsize/2).toInt)
 
-// Helper
-def calcTotalWidth(cellsize: Int, nFields: Int) = (cellsize * nFields) + 2
-def makeodd(number: Int) = (number*2) + 1
 
 // Limits
 def upperLimit(cellsize: Int, nFields: Int) = "_" * calcTotalWidth(cellsize,nFields) + eol
@@ -33,8 +57,15 @@ def fullBoard(cellsize: Int, nFields: Int, symbol: String) = (bBlockFull(cellsiz
 def fullBoardWrapped(cellsize: Int, nFields: Int, symbol: String) = limitWrap(makeodd(cellsize),nFields*2,fullBoard(makeodd(cellsize),nFields*2,symbol))
 // alle Argumente möglich ( cellsize:  wieviel pix um symbol, nFields Anzahl der Doppelfelder)
 def fullBoardWrapped2(cellsize: Int, nFields: Int, symbol: String) = limitWrap(makeodd(cellsize),nFields*2,fullBoard(makeodd(cellsize),nFields*2,symbol))
+def autoBoard(size: Int, symbol: String) = limitWrap(makeodd(size),size*2,fullBoard(makeodd(size),size*2,symbol))
 
 
+//Modified
+//def to2Tuple(entrys: List[String]) = entrys.map()
+def modLine(cellsize: Int, symbols: List[(String,String)]) = "|" + symbols.map((a,b) => innerBCell(cellsize,a) + innerWCell(cellsize,b)).mkString("") + "|" + eol
+def modBlock(cellsize: Int, nFields: Int, symbols: List[(String,String)]) = (bLine(cellsize,nFields*4) * (cellsize/2).toInt) +  modLine(cellsize,symbols) + (bLine(cellsize,nFields*4) * (cellsize/2).toInt)
+def modBoard(cellsize: Int, nFields: Int, symbols: List[List[(String,String)]]) = symbols.map(l => modBlock(cellsize,nFields/2,l)).mkString("")
+def modBoardWrapped(cellsize: Int, nFields: Int,symbols: List[List[(String,String)]]) = limitWrap(cellsize*2,nFields,modBoard(cellsize,nFields,symbols))
 
   
 
