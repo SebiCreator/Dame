c983de2953eb324bc4aac99b0e39e5d2eb3aa2a1
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-04-28 17:53:48 +0200

    Update Controller.scala

--- src/main/scala/de/htwg/se/dame/controller/Controller.scala
+++ src/main/scala/de/htwg/se/dame/controller/Controller.scala
@@ -1,23 +1,22 @@
 package de.htwg.se.dame.controller
 
-//import de.htwg.se.sudoku.model
 import de.htwg.se.dame.util.Observable
-import de.htwg.se.dame.model.Matrix
+import de.htwg.se.dame.model.*
 
 class Controller(var matrix: Matrix[String]) extends Observable {
+  var field: String = ""
   def initFill(filling: String): Unit = {
     val matrix = new Matrix[String](Nil, 8, "X", "O")
     notifyObservers
   }
 
   def tup(): Unit = {
-    val matrix = new Matrix[String].initFill() // :/
+    val matrix = new Matrix[String].initFill()
     notifyObservers
   }
 
-  def fullBoardWrapped2(cellsize: Int, nFields: Int, symbol: String): Unit = {
-    val matrix = new Matrix[String].initFill().tup()
-    fullBoardWrapped2(cellsize, nFields, symbol)
+  def startGame(cellsize: Int, n_Fields: Int): Unit = {
+    field = fullBoardWrapped2(cellsize, n_Fields, " ")
     notifyObservers
 
   }
