f87d20a87705697469fb6e80c92e48a08038727e
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-04-28 14:51:37 +0200

    Update Controller.scala

--- src/main/scala/de/htwg/se/dame/controller/Controller.scala
+++ src/main/scala/de/htwg/se/dame/controller/Controller.scala
@@ -10,9 +10,16 @@ class Controller(var matrix: Matrix[String]) extends Observable {
     notifyObservers
   }
 
-  def tupilization(): Unit = {
-    matrix = new Matrix[String].initFill() // :/
+  def tup(): Unit = {
+    val matrix = new Matrix[String].initFill() // :/
     notifyObservers
+  }
+
+  def fullBoardWrapped2(cellsize: Int, nFields: Int, symbol: String): Unit = {
+    val matrix = new Matrix[String].initFill().tup()
+    fullBoardWrapped2(cellsize, nFields, symbol)
+    notifyObservers
+
   }
 
 }
