8b9e2f20962504de7820bbf8ed47c13151a5b318
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-04-28 17:54:14 +0200

    Update Dame.scala

--- src/main/scala/de/htwg/se/dame/Dame.scala
+++ src/main/scala/de/htwg/se/dame/Dame.scala
@@ -4,9 +4,29 @@ import de.htwg.se.dame.aview.Tui
 import de.htwg.se.dame.controller.Controller
 import de.htwg.se.dame.model.Matrix
 
+import scala.io.StdIn.readLine
+
+object Dame {
+  val controller = new Controller(new Matrix)
+  val tui = new Tui(controller)
+  controller.notifyObservers
+
+  def main(args: Array[String]): Unit = {
+    var input: String = ""
+    tui.processInputLine("help")
+
+    while (input != "quit") {
+      input = readLine()
+      tui.processInputLine(input)
+    }
+  }
+}
+
+/*
 @main def hello: Unit =
 
   val controller = new Controller(new Matrix)
   val tui = new Tui(controller)
   tui.processInputLine("help")
   tui.processInputLine("custom")
+ */
