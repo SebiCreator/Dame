c9f5bf6f9994ca3e71b61900e38f35a6cde5bdfd
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-05-19 20:35:23 +0200

    Update PlayerSpec.scala

--- src/test/scala/de/htwg/se/dame/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/dame/model/PlayerSpec.scala
@@ -4,14 +4,28 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class PlayerSpec extends AnyWordSpec {
-  formatHead("A Player") when {
-    "new" should {
-      val player = Player("John Doe")
+  formatHead("A Person") when {
+    "is a Player" should {
+      val player = new Player("John Doe")
       "have a name" in {
         player.name should be("John Doe")
       }
       "have a String representation" in {
         player.toString should be("John Doe")
+      }
+      "have a counting winningStreak" in {
+        player.wins should be(0)
+      }
+      "which will increase at each win" in {
+        player.increaseWinningStreak().wins should be(1)
+      }
+    }
+  }
+  formatHead("A Spectator") when {
+    "is present " should {
+      val spec = new Spectator("Hansele")
+      "have a name" in {
+        spec.name should be("Hansele")
       }
     }
   }
