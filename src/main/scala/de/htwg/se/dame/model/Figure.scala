2bfc6278b51c8ce6a855de6ba11710c3dbb17c05
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-05-19 20:36:42 +0200

    Update Figure.scala

--- src/main/scala/de/htwg/se/dame/model/Figure.scala
+++ src/main/scala/de/htwg/se/dame/model/Figure.scala
@@ -2,27 +2,119 @@ package de.htwg.se.dame.model
 
 /*
 The figure based on the FactoryMethod-Pattern
-*/
+ */
 
 trait Figure {
-    val onBoard = true
-    def possibleMove = List()
-    def move = List()
+  val onBoard = true
+  def command(
+      matrix: Matrix,
+      command: String,
+      player: String,
+      row: Int,
+      col: Int
+  ): Matrix
+  def command_upper(matrix: Matrix, command: String, row: Int, col: Int): Matrix
+  def command_lower(matrix: Matrix, command: String, row: Int, col: Int): Matrix
+
 }
 
 private class Farmer_Figure extends Figure {
-    override def possibleMove  = List()
-    override def move =  List()
+  override def command(
+      matrix: Matrix,
+      command: String,
+      player: String,
+      row: Int,
+      col: Int
+  ) = {
+    player match {
+      case "Player1" => command_upper(matrix, command, row, col)
+
+      case "Player2" => command_lower(matrix, command, row, col)
+    }
+
+  }
+
+  override def command_upper(
+      matrix: Matrix,
+      command: String,
+      row: Int,
+      col: Int
+  ) = {
+    command match {
+      case "right" => matrix.moveRightU(row, col)
+
+      case "left" => matrix.moveLeftU(row, col)
+    }
+  }
+
+  override def command_lower(
+      matrix: Matrix,
+      command: String,
+      row: Int,
+      col: Int
+  ) = {
+    command match {
+      case "right" => matrix.moveRightL(row, col)
+
+      case "left" => matrix.moveLeftL(row, col)
+    }
+  }
 }
 
 private class Dame_Figure extends Figure {
-    override def possibleMove =  List()
-    override def move =  List()
+  override def command(
+      matrix: Matrix,
+      command: String,
+      player: String,
+      row: Int,
+      col: Int
+  ) = {
+    player match {
+      case "Player1" => command_upper(matrix, command, row, col)
+
+      case "Player2" => command_lower(matrix, command, row, col)
+    }
+
+  }
+
+  override def command_upper(
+      matrix: Matrix,
+      command: String,
+      row: Int,
+      col: Int
+  ) = {
+    command match {
+      case "right" => matrix.moveLeftU(row, col)
+
+      case "left" => matrix.moveRightU(row, col)
+
+      /* TODO
+      case "backLeft" =>
+
+      case "backRight" =>
+       */
+    }
+  }
+
+  override def command_lower(
+      matrix: Matrix,
+      command: String,
+      row: Int,
+      col: Int
+  ) = {
+    command match {
+      case "right" => matrix.moveRightL(row, col)
+
+      case "left" => matrix.moveLeftL(row, col)
+
+    }
+  }
 }
 
 object Figure {
-    def apply(kind: String) = kind match {
-        case "Dame" => new Dame_Figure()
-        case "Farmer" => new Farmer_Figure()
-    }
+  def apply(kind: String) = kind match {
+    case "Dame"   => new Dame_Figure()
+    case "Farmer" => new Farmer_Figure()
+
+  }
 }
