0e3f08cc1cf4c918f28c68fe19cadae359bcb30e
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-05-27 13:16:45 +0200

    added tests

--- src/test/scala/de/htwg/se/dame/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/dame/controller/ControllerSpec.scala
@@ -6,13 +6,36 @@ import de.htwg.se.dame.controller.Controller
 import de.htwg.se.dame.model.Matrix
 import de.htwg.se.dame.util.Observer
 import de.htwg.se.dame.model.Board
+import de.htwg.se.dame.model.*
+import org.scalatest.matchers.must.Matchers
 
-class ControllerSpec extends AnyWordSpec {
+class ControllerSpec extends AnyWordSpec with Matchers {
   "The Controller" should {
+    val board = Board()
+    val matrix = Matrix().initFill()
     val controller = Controller(None)
-    "Start Game should create a new Board with Players in place" in {
-     controller.startGame() should be (Board()) 
+    val testController = Controller(Some(matrix))
+
+    "getMatrix returns the uderlying Matrix" in {
+      controller.getMatrix() should be(None)
+      testController.getMatrix().get should be(matrix)
     }
+
+    "print the matrix" in {
+      testController.getPrintData() should be(
+        modBoardWrapped(1, matrix.cells / 2, matrix.tup())
+      )
+    }
+    "get the current player" in {
+      testController.currentPlayer() should be("Player1")
+    }
+
+    "call a move on the current board" in {
+      testController.playtest("left", 1, 0).get should be(
+        matrix.replaceCell(1, 0, 0).replaceCell(2, 1, 1).changePlayer()
+      )
+    }
+
     "notify its observers on change" in {
       class TestObserver(controller: Controller) extends Observer:
         controller.add(this)
