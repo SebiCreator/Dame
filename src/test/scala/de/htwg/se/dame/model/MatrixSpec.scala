1eef57f1f48a12ddcbec23e22927fcd5262af654
Daniel Rill <92930703+danielrill@users.noreply.github.com>
2022-05-27 13:14:49 +0200

    added tests

--- src/test/scala/de/htwg/se/dame/model/MatrixSpec.scala
+++ src/test/scala/de/htwg/se/dame/model/MatrixSpec.scala
@@ -8,8 +8,10 @@ class MatrixSpec extends AnyWordSpec with Matchers {
     "A Matrix is a tailored Data Structure to capture Board coordinate it"
   ) should {
     val default = new Matrix()
-    default.player1 should be (Player1())
+    default.player1 should be(Player1())
+    default.player1.toString should be("X")
     default.player2 should be(Player2())
+    default.player2.toString should be("X")
     val test = new Matrix()
     test.cells should be(6)
     test.data should be(Nil)
@@ -38,7 +40,7 @@ class MatrixSpec extends AnyWordSpec with Matchers {
   formatText("tup returns the Board Data as tuples for printing and") should {
     val default = new Matrix().initFill()
     val tuple = default.tup()
-    tuple(1) should be(List(("X"," "), ("X"," "), ("X"," ")))
+    tuple(1) should be(List(("X", " "), ("X", " "), ("X", " ")))
   }
   formatText(
     "replace cell changes the instance on the given coordinate and"
@@ -122,12 +124,60 @@ class MatrixSpec extends AnyWordSpec with Matchers {
     "movePossible checks if there are possible move for the according figure"
   ) should {
     val m = Matrix().initFill()
-    m.movePossible("upper","left",1,1) should be (true)
-    m.movePossible("upper","right",1,1) should be (true)
-    m.movePossible("lower","right",4,2) should be (true)
-    m.movePossible("lower","left",4,1) should be (true)
+    m.movePossible("upper", "left", 1, 1) should be(true)
+    m.movePossible("upper", "right", 1, 1) should be(true)
+    m.movePossible("lower", "right", 4, 2) should be(true)
+    m.movePossible("lower", "left", 4, 1) should be(true)
   }
 
+  formatText(
+    "whichFigure provides the Figure at a given position"
+  ) in {
+    val test = Matrix().initFill()
+    val dame1 = test.replaceCell(0, 0, 3)
+    val dame2 = test.replaceCell(1, 1, 4)
 
+    dame1.whichFigure(0, 0) should be("dame")
+    dame2.whichFigure(1, 1) should be("dame")
+    test.whichFigure(1, 0) should be("farmer")
+    test.whichFigure(4, 1) should be("farmer")
+    test.whichFigure(3, 3) should be("leer")
+  }
+
+  formatText(
+    "the killPossible commands return a bool if its possible" +
+      "and do the move"
+  ) in {
+    val testMatrix =
+      Matrix()
+        .initFill()
+        .replaceCell(2, 1, 2)
+        .replaceCell(3, 1, 1)
+        .replaceCell(3, 4, 1)
+
+    testMatrix.rightKillPossibleU(2, 3) should be(false)
+    testMatrix.rightKillPossibleU(1, 2) should be(true)
+
+    testMatrix.leftKillPossilbeU(1, 0) should be(true)
+    testMatrix.leftKillPossilbeU(1, 2) should be(false)
+
+    testMatrix.leftKillPossilbeL(3, 2) should be(false)
+    testMatrix.leftKillPossilbeL(4, 2) should be(true)
+
+    testMatrix.rightKillPossibleL(4, 3) should be(true)
+    testMatrix.rightKillPossibleL(4, 1) should be(false)
+
+    val killMatrixRU =
+      testMatrix.killRightU(1, 2).data(2) should be(List(0, 0, 0, 0, 0, 0))
+
+    val killMatrixLU =
+      testMatrix.killLeftU(1, 0).data(2) should be(List(0, 0, 0, 0, 0, 0))
+
+    val killMatrixLL =
+      testMatrix.killLeftL(4, 5).data(3) should be(List(0, 1, 0, 0, 0, 0))
+
+    val killMatrixRL =
+      testMatrix.killRightL(4, 3).data(4) should be(List(0, 2, 0, 0, 0, 2))
+  }
 
 }
