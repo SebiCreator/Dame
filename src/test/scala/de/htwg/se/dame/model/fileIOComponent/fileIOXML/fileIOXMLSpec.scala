c4d7a0a12a0e63259e4ecbc8432095f3155bc21f
SebiCreator <Sebastian.Kaeser.public@gmail.com>
2022-06-16 19:14:27 +0200

    fixed.

--- src/test/scala/de/htwg/se/dame/model/fileIOComponent/fileIOXML/fileIOXMLSpec.scala
+++ src/test/scala/de/htwg/se/dame/model/fileIOComponent/fileIOXML/fileIOXMLSpec.scala
@@ -1,1 +1,21 @@
 package de.htwg.se.dame.model.fileIOComponent.fileIOXML
+
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+import de.htwg.se.dame.model.gameComponent.gameBaseImpl.Matrix
+
+class FileIOXMLSpec extends AnyWordSpec:
+  val fileIO = FileIO()
+  val matrix = Matrix().initFill()
+
+  "The FileIO with the XML implementation" should {
+    "save a the current game and its stats in game.xml" in {
+      fileIO.save(matrix)
+    }
+
+    "load a game and its stats from game.xml" in {
+      val load = fileIO.load
+
+      load.getData().equals(matrix.getData()) should be(true)
+    }
+  }
